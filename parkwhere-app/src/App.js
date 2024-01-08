94e26c68314432692383b2975d1acf040581c9fd
nhkhai <nhkhai@gmail.com>
2024-01-08 10:40:19 +0800

    Updated support for the dotenv library for secret key storage.
    Added the project reference to add the dotenv library for secret key storage. Added comments on usage. Updated the .gitignore to avoid checking in the .env file.

--- parkwhere-app/src/App.js
+++ parkwhere-app/src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Place the shared .env file outside of the src folder in the project root directory. 
+// Define the React App API Key variable in the .env file in the following syntax (variable name can be changed). 
+//REACT_APP_API_KEY = A1234567890B0987654321C
+
+// Do not commit the .env file. 
+// To use the env object to access the environment variables in the React App, 
+// use the following call with the defined variable name defined previously above. 
+//process.env.REACT_APP_API_KEY
+
 function App() {
   return (
     <div className="App">
