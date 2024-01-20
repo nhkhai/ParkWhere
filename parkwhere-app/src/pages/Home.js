7879a875c1d68ec7d99384e93016e793180b0ec3
izzat <izzatfadzlon@gmail.com>
2024-01-20 08:14:12 +0800

    PAR-45:Implement dark mode for inner Card and SideNav + fix small bugs

--- parkwhere-app/src/pages/Home.js
+++ parkwhere-app/src/pages/Home.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import styles from "./Home.module.css";
+import ModeContext from "../context/ModeContext";
 
 function Home() {
+  const modeCtx = useContext(ModeContext);
+
   return (
-    <div className={styles.home}>
+    <div className={`${modeCtx.isLight ? styles.card : styles.dark}`}>
       <h1>ParkHere</h1>
       <h2>Welcome to the ParkWhere app! </h2>
       <h3>Click on search to start searching for available parking lots. </h3>
